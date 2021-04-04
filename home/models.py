d52daea0e8c627114aa55e1747f3b6e5a81f66fb
Max Ehlenz <maxehlen@gmail.com>
2021-04-05 00:10:41 +0200

    first_commit

---
+++ home/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
+
+class HomePage(Page):  
+
+    templates = "/home/home_page.html"
+    banner_title = models.CharField(max_length=100, blank=False, null=True)
+    max_count = 1
+
+    content_panels = Page.content_panels + [
+        FieldPanel("banner_title")
+    ]
+
+   
